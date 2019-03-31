b57934f188061bf85df2585045c79c8cfa2bbb81
Елисеев Максим <mneliseev@gmail.com>
2019-03-31 07:00:44 +0500

    Init project
    init Start project

---
+++ VKClientGB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKClientGB
+//
+//  Created by Максим on 31.03.2019.
+//  Copyright © 2019 MaksimEliseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
