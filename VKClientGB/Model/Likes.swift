c98fe69c66d5f7aa0fe7a9c78a1a7601d3d87f0a
Maksim Eliseev <mneliseev@gmail.com>
2019-08-08 14:22:44 +0500

    Некоторые изменения, которые не реализованы до конца
    Некоторые изменения, которые не реализованы до конца

---
+++ VKClientGB/Model/Likes.swift
@@ -0,0 +1,9 @@
+//
+//  Like.swift
+//  VKClientGB
+//
+//  Created by MAC on 08/08/2019.
+//  Copyright © 2019 MaksimEliseev. All rights reserved.
+//
+
+import Foundation
---
+++ VKClientGB/Services/NetworkService/LikeRequst.swift
@@ -0,0 +1,9 @@
+//
+//  LikeRequst.swift
+//  VKClientGB
+//
+//  Created by MAC on 08/08/2019.
+//  Copyright © 2019 MaksimEliseev. All rights reserved.
+//
+
+import Foundation
